c7ab2073238b61247973847bc13908f40b536906
Jarred Witt <me@jarredwitt.com>
2019-11-22 13:35:12 -0800

    Add cellRenderer

--- components/rows.js
+++ components/rows.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { View, ViewPropTypes, Text, StyleSheet } from 'react-native';
 import { Cell } from './cell';
 import { sum } from '../utils';
 
 export class Row extends Component {
   static propTypes = {
+    cellRenderer: PropTypes.func,
+    rowIndex: PropTypes.number,
     style: ViewPropTypes.style,
     textStyle: Text.propTypes.style
   };
+
+  cellRenderer = (item, i) => {
+    const { cellRenderer, widthArr, height, flexArr, rowIndex, textStyle, ...props } = this.props;
+  
+    const flex = flexArr && flexArr[i];
+    const wth = widthArr && widthArr[i];
+
+    if (cellRenderer) {
+      return cellRenderer({
+        item,
+        index: i,
+        rowIndex,
+        flex,
+        textStyle,
+        width: wth,
+      });
+    }
+
+    return <Cell key={i} data={item} width={wth} height={height} flex={flex} textStyle={textStyle} {...props} />;
+  }
 
   render() {
     const { data, style, widthArr, height, flexArr, textStyle, ...props } = this.props;
@@ -15,11 +38,7 @@ export class Row extends Component {
 
     return data ? (
       <View style={[height && { height }, width && { width }, styles.row, style]}>
-        {data.map((item, i) => {
-          const flex = flexArr && flexArr[i];
-          const wth = widthArr && widthArr[i];
-          return <Cell key={i} data={item} width={wth} height={height} flex={flex} textStyle={textStyle} {...props} />;
-        })}
+        {data.map(this.cellRenderer)}
       </View>
     ) : null;
   }
@@ -40,6 +59,7 @@ export class Rows extends Component {
       <View style={[flex && { flex }, width && { width }]}>
         {data.map((item, i) => {
           const height = heightArr && heightArr[i];
+          console.log(i)
           return (
             <Row
               key={i}
@@ -49,6 +69,7 @@ export class Rows extends Component {
               flexArr={flexArr}
               style={style}
               textStyle={textStyle}
+              rowIndex={i}
               {...props}
             />
           );
